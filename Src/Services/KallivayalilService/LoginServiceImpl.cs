07f2892d42d0b667ac3a34c7c16ffe7f61432c40
lucykur <lucykur@thoughtworks.com>
2011-05-27 08:41:32 +0530

    Adding missing file LoginServiceImpl <Lucy>

---
+++ Src/Services/KallivayalilService/LoginServiceImpl.cs
@@ -0,0 +1,27 @@
+using Kallivayalil.DataAccess.Repositories;
+
+namespace Kallivayalil
+{
+    public class LoginServiceImpl
+    {
+        private readonly LoginRepository repository;
+        private readonly EmailRepository emailRepository;
+
+        public LoginServiceImpl()
+        {
+            repository = new LoginRepository();
+            emailRepository = new EmailRepository();
+        }
+
+
+        public bool Authenticate(string username,string password)
+        {
+            var email = emailRepository.Load(username);
+            if(email ==null)
+                return false;
+
+            var login = repository.Load(email);
+            return repository.Authenticate(login, password);
+        }
+    }
+}
